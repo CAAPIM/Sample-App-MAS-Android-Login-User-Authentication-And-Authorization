d3a023ce0938f30fe0b68bc7ceb382e1fc4038d2
Naveen <yerram.kumar@broadcom.com>
2021-12-10 12:18:15 +0530

    1) Did necessary changes to make app run 2) Added UI-Automation for User-Authentication

---
+++ MASLoginUserAuthentication/app/src/androidTest/java/com/ca/mas/massample/LoginUserAuthenticationTest.java
@@ -0,0 +1,83 @@
+package com.ca.mas.massample;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.StringContains.containsString;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.IdlingRegistry;
+import androidx.test.espresso.IdlingResource;
+import androidx.test.espresso.PerformException;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.util.HumanReadables;
+import androidx.test.espresso.util.TreeIterables;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.ca.mas.ui.MASLoginActivity;
+
+import org.hamcrest.Matcher;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+
+@RunWith(AndroidJUnit4.class)
+public class LoginUserAuthenticationTest {
+
+
+    private IdlingResource mIdlingResource;
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+
+    @Test
+    public void LoginText() throws InterruptedException {
+
+        Thread.sleep(10000);
+        onView(withId(R.id.activity_mas_login_edit_text_username)).check(matches(isDisplayed()));
+        onView(withId(R.id.activity_mas_login_edit_text_username))
+                .perform(typeText(getResourceString(R.string.user_name)), closeSoftKeyboard());
+
+        onView(withId(R.id.activity_mas_login_edit_text_password))
+                .perform(typeText(getResourceString(R.string.password)), closeSoftKeyboard());
+        onView(withId(R.id.activity_mas_login_button_login)).perform(click());
+
+        Thread.sleep(8000);
+
+        onView(withId(R.id.apiResponse))
+                .check(matches(withText("{\"products\":[{\"id\":1,\"name\":\"AC544\",\"Time 13:00\":\"On Time\"},{\"id\":2,\"name\":\"WJ6345\",\"Arrival Time 14:55\":\"Delayed\"},{\"id\":3,\"name\":\"BA2490\",\"Arrival Time 14:55\":\"On Time\"},{\"id\":4,\"name\":\"AA1452\",\"Arrival Time 15:15\":\"Early Arrival\"},{\"id\":5,\"name\":\"WJ2678\",\"Arrival Time 16:32\":\"Delayed\"},{\"id\":6,\"name\":\"AC4455\",\"Arrival Time 17:45\":\"On Time\"}],\"device_geo\":\"\",\"clientCert.subject\":\"CN=admin, OU=6c639b93a7a6dc2b98f37326b261512b3f25c060c1d754540a3f857f968ef49d, DC=AOSPonIAEmulator, O=Broadcom\"}")));
+
+    }
+
+    private String getResourceString(int id) {
+        Context targetContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        return targetContext.getResources().getString(id);
+    }
+
+
+}
--- MASLoginUserAuthentication/app/src/main/java/com/ca/mas/massample/MainActivity.java
+++ MASLoginUserAuthentication/app/src/main/java/com/ca/mas/massample/MainActivity.java
@@ -2,73 +2,245 @@ package com.ca.mas.massample;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.test.espresso.IdlingResource;
+
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ca.mas.core.error.TargetApiException;
 import com.ca.mas.core.service.MssoIntents;
+import com.ca.mas.core.storage.Storage;
+import com.ca.mas.core.storage.StorageException;
+import com.ca.mas.core.storage.implementation.MASStorageManager;
+import com.ca.mas.core.store.ClientCredentialContainer;
+import com.ca.mas.core.store.OAuthTokenContainer;
+import com.ca.mas.core.store.StorageProvider;
+import com.ca.mas.core.store.TokenManager;
 import com.ca.mas.foundation.MAS;
 import com.ca.mas.foundation.MASAuthenticationListener;
 import com.ca.mas.foundation.MASCallback;
+import com.ca.mas.foundation.MASConfiguration;
 import com.ca.mas.foundation.MASDevice;
 import com.ca.mas.foundation.MASOtpAuthenticationHandler;
 import com.ca.mas.foundation.MASRequest;
+import com.ca.mas.foundation.MASRequestBody;
 import com.ca.mas.foundation.MASResponse;
+import com.ca.mas.foundation.MASResponseBody;
 import com.ca.mas.foundation.MASUser;
 import com.ca.mas.foundation.auth.MASAuthenticationProviders;
 import com.ca.mas.ui.MASLoginActivity;
 
+
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.security.cert.X509Certificate;
+import java.util.concurrent.ExecutionException;
 
+import sun.util.calendar.LocalGregorianCalendar;
+
+@SuppressWarnings("ALL")
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MENU_GROUP_LOGOUT = 66;
+    private static final int MENU_ITEM_LOG_OUT = 3;
+    private static final int MENU_ITEM_REMOVE_DEVICE_REGISTRATION = 4;
+    private static final int MENU_ITEM_DESTROY_TOKEN_STORE = 2;
+    int versionCode = 0;
+    String versionName = null;
+    private TextView mTvVersion = null;
+    TextView apiResponse;
+    private OAuthTokenContainer privateTokens;
+    private final String content = "{\"test\":\"value\"}";
+    private final String jwtPath = "/jwtVerification";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTvVersion = (TextView) findViewById(R.id.app_version);
+        apiResponse = (TextView) findViewById(R.id.apiResponse);
+
+        try {
+            PackageInfo packageInfo = this.getPackageManager()
+                    .getPackageInfo(this.getPackageName(), 0);
+            versionCode = packageInfo.versionCode;
+            versionName = packageInfo.versionName;
+
+            Log.d("ESCALATION", "Current App version code::: " + versionCode);
+
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.d("ESCALATION", "Package name not found");
+        }
 
         MAS.start(this);
+        MAS.debug();
+        //MAS.DEBUG("MAS", Log.d(""));
 
-        MAS.setAuthenticationListener(new MASAuthenticationListener() {
+        TokenManager mgr = StorageProvider.getInstance().getTokenManager();
+        mTvVersion.setText("App Version code " + versionCode + "\n" + "Version Name::: " + versionName);
+
+//        if(versionCode == 18) {
+//            Log.d("ESCALATION", "Version code matched");
+//
+////            Log.d("ESCALATION", "Before Upgrade::::Token store verification::: " + mgr.isTokenStoreReady());
+////            Log.d("ESCALATION", "Before Upgrade::::IDToken::: " + mgr.getIdToken());
+////            Log.d("ESCALATION", "Before Upgrade::::MAG Identifier::: " + mgr.getMagIdentifier());
+////            Log.d("ESCALATION", "Before Upgrade::::User Profile::: " + mgr.getUserProfile());
+////            Log.d("ESCALATION", "Before Upgrade::::Private key::: " + mgr.getClientPrivateKey());
+////            Log.d("ESCALATION", "Before Upgrade::::Public key::: " + mgr.getClientPublicKey());
+////            Log.d("ESCALATION", "After Upgrade::::Client certificate::: "+mgr.getClientCertificateChain().toString());
+//
+//            //printStorageProviderLogs();
+//            doResetLocally();
+//
+//        }
+        //else if(versionCode == 19)
+        // {
+
+//            doResetLocally();
+        // printStorageProviderLogs();
+//            Log.d("ESCALATION", "Version code matched");
+//            Log.d("ESCALATION", "After Upgrade::::Token store verification::: "+mgr.isTokenStoreReady());
+//            Log.d("ESCALATION", "After Upgrade::::IDToken::: "+mgr.getIdToken());
+//            Log.d("ESCALATION", "After Upgrade::::MAG Identifier::: "+mgr.getMagIdentifier());
+//            Log.d("ESCALATION", "After Upgrade::::User Profile::: "+mgr.getUserProfile());
+//            Log.d("ESCALATION", "After Upgrade::::Private key::: "+mgr.getClientPrivateKey());
+//            Log.d("ESCALATION", "After Upgrade::::Public key::: "+mgr.getClientPublicKey());
+//            Log.d("ESCALATION", "After Upgrade::::Client certificate::: "+mgr.getClientCertificateChain().toString());
+        //mgr.getClientCertificateChain();
+
+//            try {
+//                final Storage storage = new MASStorageManager().getStorage(MASStorageManager.MASStorageType.TYPE_KEYSTORE, new Object[]{getApplicationContext(), false});
+//                storage.getAllKeys().getData();
+//
+//            } catch (StorageException e) {
+//                if (e.getCode() == StorageException.STORE_NOT_UNLOCKED) {
+//                    Log.d("ESCALATION", "KEYSTORE  is Locked!");
+//                } else {
+//                    Log.d("ESCALATION", "KEYSTORE Storage instantiation failed " + e);
+//                }
+//
+//            }
+        //}
+
+//        showMessage("MAG Identifier:::"+mgr.getMagIdentifier()+" \n"+"IDToken::: "+mgr.getIdToken(), Toast.LENGTH_LONG);
+//        MAS.setAuthenticationListener(new MASAuthenticationListener() {
+//            @Override
+//            public void onAuthenticateRequest(Context context, long requestId, MASAuthenticationProviders providers) {
+//                Intent intent = new Intent(context, MASLoginActivity.class);
+//                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                intent.putExtra(MssoIntents.EXTRA_REQUEST_ID, requestId);
+//                intent.putExtra(MssoIntents.EXTRA_AUTH_PROVIDERS, providers);
+//                context.startActivity(intent);
+//            }
+//
+//            @Override
+//            public void onOtpAuthenticateRequest(Context context, MASOtpAuthenticationHandler handler) {
+//                //Ignore for now
+//            }
+//        });
+
+//        MASUnitCallbackFuture<MASResponse<JSONObject>> callback = new MASUnitCallbackFuture<>();
+//        MASRequest request = null;
+//        try {
+//            request = new MASRequest.MASRequestBuilder(new URI(jwtPath))
+//                    .post(MASRequestBody.jsonBody(new JSONObject(content)))
+//                    .sign()
+//                    .notifyOnCancel()
+//                    .build();
+//        } catch (URISyntaxException e) {
+//            e.printStackTrace();
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//
+//        MAS.invoke(request, callback);
+//        MASResponseBody<JSONObject> jsonResponseBody = null;
+//        try {
+//            jsonResponseBody = callback.get().getBody();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        } catch (ExecutionException e) {
+//            e.printStackTrace();
+//        }
+//        JSONObject json = jsonResponseBody.getContent();
+////        assertEquals(json.get("valid"), true);
+//        try {
+//            JSONObject payload = json.getJSONObject("payload");
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+
+        /*MASUser.login("admin", "7layer".toCharArray(), new MASCallback<MASUser>() {
             @Override
-            public void onAuthenticateRequest(Context context, long requestId, MASAuthenticationProviders providers) {
-                Intent intent = new Intent(context, MASLoginActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.putExtra(MssoIntents.EXTRA_REQUEST_ID, requestId);
-                intent.putExtra(MssoIntents.EXTRA_AUTH_PROVIDERS, providers);
-                context.startActivity(intent);
-            }
-
-            @Override
-            public void onOtpAuthenticateRequest(Context context, MASOtpAuthenticationHandler handler) {
-                //Ignore for now
-            }
-        });
-
-        MASUser.login(new MASCallback<MASUser>() {
-            @Override
-            public void onSuccess(MASUser result) {
-                Log.w("TUTORIAL", "Logged in as " + MASDevice.getCurrentDevice().getIdentifier());
-
+            public void onSuccess(MASUser user) {
+                Log.d("ESCALATION", "Logged in as:: Identifier:: " + MASDevice.getCurrentDevice().getIdentifier());
+                Log.d("ESCALATION", "Login in as: User Name:: " +MASUser.getCurrentUser().getUserName());
                 getFlightList();
-
             }
 
             @Override
             public void onError(Throwable e) {
-                Log.w("TUTORIAL", "Login failure: " + e);
+            }
+        });*/
+        MASUser.login(new MASCallback<MASUser>() {
+            @Override
+            public void onSuccess(MASUser result) {
+                Log.d("ESCALATION", "Logged in as:: Identifier:: " + MASDevice.getCurrentDevice().getIdentifier());
+                Log.d("ESCALATION", "Login in as: User Name:: " + MASUser.getCurrentUser().getUserName());
+                getFlightList();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d("ESCALATION", "Login failure: " + e);
             }
         });
 
     }
+
+
+//    public void printStorageProviderLogs(){
+//        TokenManager mgr = StorageProvider.getInstance().getTokenManager();
+////        Log.d("ESCALATION", "After Upgrade::::Token store verification::: "+mgr.isTokenStoreReady());
+////        Log.d("ESCALATION", "After Upgrade::::Token store verification::: "+mgr.getClientPrivateKey());
+////        Log.d("ESCALATION", "After Upgrade::::IDToken Type::: "+mgr.getIdToken().getType());
+////        Log.d("ESCALATION", "After Upgrade::::IDToken value::: "+mgr.getIdToken().getValue());
+////        Log.d("ESCALATION", "After Upgrade::::MAG Identifier::: "+mgr.getMagIdentifier());
+////        Log.d("ESCALATION", "After Upgrade::::User Profile::: "+mgr.getUserProfile());
+////        Log.d("ESCALATION", "After Upgrade::::Private key::: "+mgr.getClientPublicKey());
+//        ClientCredentialContainer clientCredentialContainer = StorageProvider.getInstance().getClientCredentialContainer();
+////        Log.d("ESCALATION", "After Upgrade::::ClientId::: "+clientCredentialContainer.getClientId());
+////        Log.d("ESCALATION", "After Upgrade::::ClientSecret::: "+clientCredentialContainer.getClientSecret());
+//        OAuthTokenContainer oAuthTokenContainer = StorageProvider.getInstance().getOAuthTokenContainer();
+////        Log.d("ESCALATION", "After Upgrade::::AccessToken::: "+oAuthTokenContainer.getAccessToken());
+////        Log.d("ESCALATION", "After Upgrade::::RefreshToke::: "+oAuthTokenContainer.getRefreshToken());
+////        X509Certificate[] chain = mgr.getClientCertificateChain();
+////        for (int i = 0; i < chain.length; i++) {
+////            Log.d("ESCALATION", "After Upgrade::::MAG client Certificate Chain=" + i + " ->" + chain[i]);
+////            chain[i].getSignature();
+////        }
+//    }
 
     public void getFlightList() {
         try {
@@ -84,24 +256,28 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void onSuccess(MASResponse<JSONObject> result) {
-                    Log.w("TUTORIAL", "Getting Flight List, got result: " + result.getBody().getContent());
+                    Log.d("ESCALATION", "Getting Flight List, got result: " + result.getBody().getContent());
+                    apiResponse.setText(result.getBody().getContent().toString());
                     showMessage("Airline info: " + result.getBody().getContent(), Toast.LENGTH_LONG);
                 }
 
                 @Override
                 public void onError(Throwable e) {
-                    Log.w("TUTORIAL", "Getting Flight List, got error: " + e);
+                    Log.d("ESCALATION", "Getting Flight List, got error: " + e);
                     if (e.getCause() instanceof TargetApiException) {
+                        apiResponse.setText(new String(((TargetApiException) e.getCause()).getResponse()
+                                .getBody().getRawContent()));
                         showMessage(new String(((TargetApiException) e.getCause()).getResponse()
                                 .getBody().getRawContent()), Toast.LENGTH_SHORT);
                     } else {
+                        apiResponse.setText("Error: " + e.getMessage());
                         showMessage("Error: " + e.getMessage(), Toast.LENGTH_LONG);
                     }
                 }
             });
 
         } catch (Exception x) {
-            Log.w("TUTORIAL", "ERROR Getting Flight List");
+            Log.d("ESCALATION", "ERROR Getting Flight List");
         }
 
     }
@@ -132,11 +308,57 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.Settings) {
+            return true;
+        } else if (id == R.id.Logout) {
+            doServerLogout();
+            return true;
+        } else if (id == R.id.Reset) {
+            doResetLocally();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    private void doResetLocally() {
+//        MASDevice.getCurrentDevice().resetLocally();
+//        Log.d("ESCALATION", "Reset locally is completed");
+//        showMessage("Device Registration Destroyed (client only)", Toast.LENGTH_LONG);
+
+        TokenManager mgr = StorageProvider.getInstance().getTokenManager();
+
+        privateTokens = StorageProvider.getInstance().getOAuthTokenContainer();
+
+        Log.d("ESCALATION", "After reset locally performed::::: Token store verification::: " + mgr.isTokenStoreReady());
+        Log.d("ESCALATION", "After reset locally performed:::::IDToken::: " + mgr.getIdToken());
+        Log.d("ESCALATION", "After reset locally performed:::::AccessToken::: " + privateTokens.getAccessToken());
+        Log.d("ESCALATION", "After reset locally performed:::::Refesh Token::: " + privateTokens.getRefreshToken());
+        Log.d("ESCALATION", "After reset locally performed:::::Expiry::: " + privateTokens.getExpiry());
+        Log.d("ESCALATION", "After reset locally performed:::::MAG Identifier::: " + mgr.getMagIdentifier());
+//        Log.d("ESCALATION", "After reset locally performed:::::User Profile::: "+mgr.getUserProfile());
+//        Log.d("ESCALATION", "After reset locally performed:::::Private key::: "+mgr.getClientPublicKey());
+
+    }
+
+    // Log the user out of all client apps and notify the server to revoke tokens.
+    private void doServerLogout() {
+        //clearItem();
+        if (MASUser.getCurrentUser() != null) {
+            MASUser.getCurrentUser().logout(new MASCallback<Void>() {
+                @Override
+                public void onSuccess(Void result) {
+                    showMessage("Successful Logout", Toast.LENGTH_LONG);
+                }
+
+                @Override
+                public void onError(Throwable e) {
+                    showMessage("Fail Logout", Toast.LENGTH_SHORT);
+                }
+            });
+        }
+    }
+
 
 }
